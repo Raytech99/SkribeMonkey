b74a17633b3e75810d3c48e91a837e7da8595373
Rayyan Jamil <raymanatl@gmail.com>
2025-03-14 18:36:09 -0400

    Adding example

---
+++ test/widget_test.dart
@@ -0,0 +1,35 @@
+import 'dart:typed_data';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:skribemonkey/external_apis/summarization_methods.dart';
+import 'package:skribemonkey/external_apis/transcription_methods.dart';
+import 'package:skribemonkey/supabase/db_methods.dart';
+
+void main() {
+  test('should summarize some text from a selected mp3 file', () async {
+    String? transcription = await TranscriptionMethods()
+        .transcribeMp3FromAssets('assets/yapsesh.mp3');
+
+    // Print the transcription
+    if (transcription != null) {
+      print(transcription);
+    } else {
+      print("Transcription failed.");
+    }
+
+    // Call the summarization method
+    final summary =
+        await SummarizationMethods().summarizeTranscript(transcription!);
+
+    // Print the summary (for debugging purposes)
+    print(summary);
+
+    // Check if the response is not null or empty
+    expect(summary, isNotNull);
+    expect(summary, isNotEmpty);
+  });
+
+  test('query patient by doc id', () async {
+    await DatabaseMethods()
+        .fetchPatientByDoctor('bc523e4f-347a-491e-bf07-af25ca844a64');
+  });
+}
